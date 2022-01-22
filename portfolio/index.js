c7fd7560d769a805f151a32e57e7702860711d08
4yd1K <papirys13@yandex.by>
2022-01-22 17:03:22 +0300

    feat: fix right menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -9,11 +9,13 @@ function toggleMenu() {
 
 //   rMenu.classList.toggle('.open-menu');
 }
-hamburger.addEventListener('click', toggleMenu);
-hamburger.addEventListener('click', openMenu);
-// rMenu.addEventListener('click', toggleMenu);
 function openMenu(){
     rMenu.classList.toggle('open-menu');
 }
 
-rMenu.addEventListener('click', openMenu);
+hamburger.addEventListener('click', toggleMenu);
+hamburger.addEventListener('click', openMenu);
+// rMenu.addEventListener('click', toggleMenu);
+
+
+// rMenu.addEventListener('click', openMenu);
