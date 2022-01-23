f297851b7115f0e45ff3339402d86b9aa3a9f869
4yd1K <papirys13@yandex.by>
2022-01-23 23:49:04 +0300

    feat: now links can close menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,5 +1,10 @@
 const hamburger = document.querySelector('.hamburger');
 const rMenu = document.querySelector('.right-menu');
+const rightMenuFirstlink = document.querySelector('.R1')
+const rightMenuSecondlink = document.querySelector('.R2')
+const rightMenuThirdlink = document.querySelector('.R3')
+const rightMenuFourthlink = document.querySelector('.R4')
+const rightMenuFifthlink = document.querySelector('.R5')
 
 console.log(hamburger);
 console.log(rMenu);
@@ -15,6 +20,18 @@ function openMenu(){
 
 hamburger.addEventListener('click', toggleMenu);
 hamburger.addEventListener('click', openMenu);
+rightMenuFirstlink.addEventListener('click', openMenu)
+rightMenuSecondlink.addEventListener('click',openMenu)
+rightMenuThirdlink.addEventListener('click', openMenu)
+rightMenuFourthlink.addEventListener('click',openMenu)
+rightMenuFifthlink.addEventListener('click', openMenu)
+
+rightMenuFirstlink.addEventListener('click', toggleMenu)
+rightMenuSecondlink.addEventListener('click',toggleMenu)
+rightMenuThirdlink.addEventListener('click', toggleMenu)
+rightMenuFourthlink.addEventListener('click', toggleMenu)
+rightMenuFifthlink.addEventListener('click', toggleMenu)
+
 // rMenu.addEventListener('click', toggleMenu);
 
 
