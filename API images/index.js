4b0279b30f654fe5cbdbb184e71537db06841c7f
4yd1K <papirys13@yandex.by>
2022-02-12 19:41:57 +0300

    feat: add lang switcher

--- API images/index.js
+++ API images/index.js
@@ -1,8 +1,26 @@
 const button = document.querySelector(".btn");
 const qt = document.querySelector(".qoute-text");
 const qa = document.querySelector(".quote-author");
-const quotes = 'https://api.icndb.com/jokes/random';
-let randomNumber = 0;
+let quotes = 'quotes.json';
+const rus = document.querySelector(".ru");
+const bel = document.querySelector(".be");
+
+function switchLang (event) {
+    // event.target.classList.toggle("active-lang");
+    if (event.target.classList.contains('ru')){
+        bel.classList.remove('active-lang');
+        rus.classList.add('active-lang');
+        quotes = 'quotes.json';
+    }
+    if (event.target.classList.contains('be')){
+        quotes = 'belarusian_quotes.json';
+        rus.classList.remove('active-lang');
+        bel.classList.add('active-lang');
+    }
+    getQuotes()
+}
+bel.addEventListener('click', switchLang );
+rus.addEventListener('click', switchLang );
 
 async function getQuotes() {
   try {
@@ -10,11 +28,14 @@ async function getQuotes() {
         const res = await fetch(quotes);
         const data = await res.json(); 
         console.log(data);
-        // randomNumber = Math.round(Math.random() * data.length);
+        randomNumber = Math.round(Math.random() * data.length);
         // console.log(data[randomNumber]);
         // console.log(randomNumber);
-        qt.textContent = data.value.joke;
-        // qa.textContent = data[randomNumber].author;
+
+        // метод js который добавляет текст в html элемент
+        // qt.textContent = data.value.joke;
+        qt.textContent = data[randomNumber].text;
+        qa.textContent = data[randomNumber].author;
 
 
   } catch (error) {
